b41e711fe4141961dc7da4848551be9c665a17dd
Johann Höchtl <johann.hoechtl@gmail.com>
2016-03-21 19:04:58 +0100

    RESTFul service creation

--- bevaddress.go
+++ bevaddress.go
@@ -3,67 +3,117 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 
 	"github.com/emicklei/go-restful"
-	"github.com/lib/pq"
+	_ "github.com/lib/pq"
 )
+
+type Address struct {
+	PLZ, Gemeindename, Ortsname, Strassenname, Hausnr *string
+	LatlongX, LatlongY                                *float64
+}
+
+func main() {
+	info("starting up")
+	conn, err := getDatabaseConnection()
+	if err != nil {
+		fatal("Error %s", err)
+	}
+	connection := &connection{DB: conn}
+
+	restful.DefaultRequestContentType(restful.MIME_JSON)
+	restful.DefaultResponseContentType(restful.MIME_JSON)
+
+	ws := new(restful.WebService)
+	ws.
+		Path("/search").
+		Produces(restful.MIME_JSON)
+
+	ws.Route(ws.GET("/fulltext/{pattern}").To(connection.fulltextSearch).
+		Doc("").
+		Param(ws.PathParameter("pattern", "search term").DataType("string")).
+		Writes([]Address{}))
+
+	cors := restful.CrossOriginResourceSharing{
+		ExposeHeaders:  []string{"X-My-Header"},
+		AllowedHeaders: []string{"Content-Type", "Accept"},
+		AllowedMethods: []string{"GET", "POST"},
+		CookiesAllowed: false}
+
+	ws.Filter(cors.Filter)
+	restful.Add(ws)
+	http.ListenAndServe(":8080", nil)
+}
+
+type connection struct {
+	*sql.DB
+}
+
+const fulltextSearchSQL = `select addritems.plz, addritems.gemeindename, addritems.ortsname, addritems.strassenname, adresse.hausnrzahl1, ST_Y(adresse.latlong), ST_X(adresse.latlong)
+from addritems
+inner join adresse
+on adresse.adrcd = addritems.adrcd
+inner join strasse
+on adresse.skz = strasse.skz
+and adresse.gkz = strasse.gkz
+and adresse.gkz = strasse.gkz
+inner join ortschaft
+on adresse.okz = ortschaft.okz
+and adresse.gkz = ortschaft.gkz
+inner join gemeinde
+on adresse.gkz = gemeinde.gkz
+where search @@ to_tsquery(plainto_tsquery('german', $1)::text || ':*')
+limit 25;`
+
+func (con *connection) fulltextSearch(request *restful.Request, response *restful.Response) {
+	param := request.PathParameter("pattern")
+	fmt.Println(param)
+	rows, err := con.Query(fulltextSearchSQL, param)
+	if err != nil {
+		fatal(err.Error())
+	}
+	defer rows.Close()
+
+	var plz, gemeindename, ortsname, strassenname, hausnrzahl1 *string
+	var latlongy, latlongx *float64
+
+	var addresses []Address
+
+	for rows.Next() {
+		if err := rows.Scan(&plz, &gemeindename, &ortsname, &strassenname, &hausnrzahl1, &latlongy, &latlongx); err != nil {
+			fatal(err.Error())
+		}
+
+		addr := Address{PLZ: plz, Gemeindename: gemeindename, Ortsname: ortsname, Strassenname: strassenname, Hausnr: hausnrzahl1, LatlongY: latlongy, LatlongX: latlongx}
+
+		addresses = append(addresses, addr)
+	}
+	response.WriteEntity(addresses)
+}
 
 func getDatabaseConnection() (*sql.DB, error) {
 
-	var dburl, dbconnstring string
+	var dburl string
 
 	if dburl = os.Getenv("DATABASE_URL"); dburl == "" {
 		dburl = "postgres://"
 	}
 
-	dbconnstring, err := pq.ParseURL(dburl)
-	if err != nil {
-		return nil, fmt.Errorf("Invalid Database Url: %s (%s)\n", dburl, err)
-	}
-
-	db, err := sql.Open("postgres", dbconnstring)
+	db, err := sql.Open("postgres", dburl)
 	if err != nil {
 		return nil, err
 	}
 	return db, nil
 }
 
-type User struct {
-	Name string
+// Log wrappers
+func info(template string, values ...interface{}) {
+	log.Printf("[bevaddress][info] "+template+"\n", values...)
 }
 
-func main() {
-	conn, err := getDatabaseConnection()
-	if err != nil {
-		fmt.Printf("Error %s", err)
-	}
-	row := conn.QueryRow("SELECT localtime")
-
-	var t pq.NullTime
-	if err := row.Scan(&t); err != nil {
-		fmt.Printf("Error %s", err)
-		return
-	}
-	fmt.Println(t)
-
-	ws := new(restful.WebService)
-
-	ws.
-		Path("/users").
-		Produces(restful.MIME_JSON)
-
-	ws.Route(ws.GET("/{user-id}").To(findUser).
-		Doc("get a user").
-		Param(ws.PathParameter("user-id", "identifier of the user").DataType("string")).
-		Writes(User{}))
-
-	restful.Add(ws)
-	http.ListenAndServe(":8080", nil)
-}
-
-func findUser(request *restful.Request, response *restful.Response) {
-	id := request.PathParameter("user-id")
-	response.WriteAsJson(User{Name: id + "world"})
+func fatal(template string, values ...interface{}) {
+	log.Fatalf("[bevaddress][fatal] "+template+"\n", values...)
 }
